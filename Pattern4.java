fcb52cb71730b03503053b3debfb450d48fb35a9
Vrutik Jadav <vrutikjadav013@gmail.com>
2021-05-01 07:50:17 +0530

    intial commit

---
+++ Pattern4.java
@@ -0,0 +1,36 @@
+package loops;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+//		for(int i=n;i>=1;i--)
+//		{
+//			for(int j=1;j<=i-1;j++)
+//			{
+//				System.out.print("  ");
+//			}
+//			for(int j=n;j>i;j--)
+//			{
+//				System.out.print(" *  ");
+//			}
+//			System.out.println();
+//		}
+    int num=1;
+		for(int i=n;i>=1;i--)
+			{
+				for(int j=1;j<=i-1;j++)
+				{
+					System.out.print("  ");
+				}
+				for(int j=n;j>i;j--)
+				{
+					System.out.print(" "+num++ +"  ");
+				}
+				System.out.println();
+			}
+
+	}
+
+}
